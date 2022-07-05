691e574b0120cd7be625d034db102b21c5878771
Nikolay Vinogradov <was.bic@gmail.com>
2022-07-05 21:26:06 +0300

    Выполнено задание. Урок 11

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ const text = document.getElementById('text');
 const textSpan = document.getElementById('text-span');
 const btn = document.getElementById('btn');
 const square = document.getElementById('square');
+const e_btn = document.getElementById('e_btn');
+const range = document.getElementById('range');
+const rangeSpan = document.getElementById('range-span');
+const circle = document.getElementById('circle');
 
-const toggler = function (e) {
-  console.dir(e.target);
+const bgrc = function (e) {
+  console.log(e.target);
 };
 
-text.addEventListener('input', toggler);
+text.addEventListener('input', bgrc);
 
 btn.addEventListener('click', function(){
   square.classList.add(text.value);
 });
 
+e_btn.style.display = 'none';
 
 
+const size = function (e) {
+  circle.style.height = range.value + '%';
+  circle.style.width = range.value + '%';
+};
+
+range.addEventListener('input', size);
+range.addEventListener('change', size);
